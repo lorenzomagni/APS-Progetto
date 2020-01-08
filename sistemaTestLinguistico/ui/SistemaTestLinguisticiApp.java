9f4024595e8b9044f1dc354d3518b5593f6e72d9
Lorenzo <ishuddlejoker@gmail.com>
2020-01-08 22:04:56 +0100

    Add files via upload

---
+++ sistemaTestLinguistico/ui/SistemaTestLinguisticiApp.java
@@ -0,0 +1,12 @@
+package sistemaTestLinguistico.ui;
+
+import sistemaTestLinguistico.ui.view.LinguisticTestsTUI;
+
+public class SistemaTestLinguisticiApp {
+	
+	//inizializzo l'UI dell'app e la faccio partire
+	public static void main(String[] args) {
+		LinguisticTestsTUI linguisticTestsTUI = new LinguisticTestsTUI();
+		linguisticTestsTUI.start();
+	}
+}
